f9398f7320ac0c08469526c66feb985bb131327d
fbara <fbara1@yahoo.com>
2021-11-01 11:58:25 -0500

    Initial Commit

---
+++ SwiftUIAdvanced/SwiftUIAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedApp.swift
+//  SwiftUIAdvanced
+//
+//  Created by Frank Bara on 11/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
